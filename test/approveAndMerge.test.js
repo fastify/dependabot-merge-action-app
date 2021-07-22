658c6d92ebfc17b99eb636291819f25d70175ba9
Simone Busoli <simone.busoli@gmail.com>
2021-07-22 17:53:04 +0200

    chore: log repository name where action runs

--- test/approveAndMerge.test.js
+++ test/approveAndMerge.test.js
@@ -1,10 +1,17 @@
 'use strict'
 
 const tap = require('tap')
-const proxyquire = require("proxyquire")
+const proxyquire = require('proxyquire')
 const assert = require('assert')
 
-let mockCreateAppJWT, mockGetInstallationRepositories, mockCreateInstallationAccessToken, mockGetRepositoryInstallation, mockGetPullRequest, mockApprovePullRequest, mockMergePullRequest
+let mockRequest,
+  mockCreateAppJWT,
+  mockGetInstallationRepositories,
+  mockCreateInstallationAccessToken,
+  mockGetRepositoryInstallation,
+  mockGetPullRequest,
+  mockApprovePullRequest,
+  mockMergePullRequest
 
 const approveAndMerge = proxyquire('../lib/approveAndMerge', {
   './github.js': {
@@ -15,22 +22,26 @@ const approveAndMerge = proxyquire('../lib/approveAndMerge', {
     getPullRequest: () => mockGetPullRequest(),
     approvePullRequest: () => mockApprovePullRequest(),
     mergePullRequest: () => mockMergePullRequest(),
-  }
+  },
 })
 
 tap.test('approveAndMerge function', t => {
-
   t.beforeEach(() => {
-    mockCreateAppJWT = (appId, privateKey) => { }
-    mockGetInstallationRepositories = async (token) => [{ name: 'the-repo', owner: { login: 'the-owner' } }]
-    mockCreateInstallationAccessToken = async (installation, appJWT) => ({ token: 'the-access-token' })
-    mockGetRepositoryInstallation = async (owner, repo, appJWT) => { }
-    mockGetPullRequest = async (owner, repo, pullRequestNumber, accessToken) => ({
-      user: { login: 'dependabot[bot]' },
-      head: { ref: "dependabot/npm_and_yarn/pkg-0.0.1" }
+    mockRequest = { log: { info: () => {} } }
+    mockCreateAppJWT = () => {}
+    mockGetInstallationRepositories = async () => [
+      { name: 'the-repo', owner: { login: 'the-owner' } },
+    ]
+    mockCreateInstallationAccessToken = async () => ({
+      token: 'the-access-token',
     })
-    mockApprovePullRequest = async (owner, repo, pullRequestNumber, accessToken, approveComment) => { }
-    mockMergePullRequest = async (owner, repo, pullRequestNumber, accessToken, mergeMethod) => { }
+    mockGetRepositoryInstallation = async () => {}
+    mockGetPullRequest = async () => ({
+      user: { login: 'dependabot[bot]' },
+      head: { ref: 'dependabot/npm_and_yarn/pkg-0.0.1' },
+    })
+    mockApprovePullRequest = async () => {}
+    mockMergePullRequest = async () => {}
   })
 
   t.plan(5)
@@ -41,13 +52,21 @@ tap.test('approveAndMerge function', t => {
     const pullRequestNumber = 123
     const options = { excludePackages: [] }
 
-    await approveAndMerge(githubToken, appJWT, pullRequestNumber, options)
+    await approveAndMerge(
+      mockRequest,
+      githubToken,
+      appJWT,
+      pullRequestNumber,
+      options
+    )
 
     t.pass()
   })
 
   t.test('should throw error on missing app installation in repo', async t => {
-    mockGetRepositoryInstallation = () => { assert(false, '{"message":"Integration not found"}') }
+    mockGetRepositoryInstallation = () => {
+      assert(false, '{"message":"Integration not found"}')
+    }
 
     const githubToken = 'the-github-token'
     const appJWT = 'jwt-token'
@@ -55,26 +74,44 @@ tap.test('approveAndMerge function', t => {
     const options = { excludePackages: [] }
 
     try {
-      await approveAndMerge(githubToken, appJWT, pullRequestNumber, options)
+      await approveAndMerge(
+        mockRequest,
+        githubToken,
+        appJWT,
+        pullRequestNumber,
+        options
+      )
       t.fail('approveAndMerge is suppose to throw an error')
     } catch (error) {
       t.match(error.message, /Integration not found/)
     }
   })
 
-  t.test('should return a message if the author of the PR is not dependabot', async t => {
-    mockGetPullRequest = async (owner, repo, pullRequestNumber, accessToken) => ({
-      user: { login: 'spambot' },
-      head: { ref: "dependabot/npm_and_yarn/pkg-0.0.1" }
-    })
+  t.test(
+    'should return a message if the author of the PR is not dependabot',
+    async t => {
+      mockGetPullRequest = async () => ({
+        user: { login: 'spambot' },
+        head: { ref: 'dependabot/npm_and_yarn/pkg-0.0.1' },
+      })
 
-    const githubToken = 'the-github-token'
-    const appJWT = 'jwt-token'
-    const pullRequestNumber = 123
-    const options = { excludePackages: [] }
+      const githubToken = 'the-github-token'
+      const appJWT = 'jwt-token'
+      const pullRequestNumber = 123
+      const options = { excludePackages: [] }
 
-    t.equal(await approveAndMerge(githubToken, appJWT, pullRequestNumber, options), 'Not dependabot PR, skipping')
-  })
+      t.equal(
+        await approveAndMerge(
+          mockRequest,
+          githubToken,
+          appJWT,
+          pullRequestNumber,
+          options
+        ),
+        'Not dependabot PR, skipping'
+      )
+    }
+  )
 
   t.test('should return a message if package has to be skipped', async t => {
     const githubToken = 'the-github-token'
@@ -82,7 +119,16 @@ tap.test('approveAndMerge function', t => {
     const pullRequestNumber = 123
     const options = { excludePackages: ['pkg'] }
 
-    t.equal(await approveAndMerge(githubToken, appJWT, pullRequestNumber, options), 'pkg is excluded, skipping')
+    t.equal(
+      await approveAndMerge(
+        mockRequest,
+        githubToken,
+        appJWT,
+        pullRequestNumber,
+        options
+      ),
+      'pkg is excluded, skipping'
+    )
   })
 
   t.test('should return a message on approve only option', async t => {
@@ -91,6 +137,15 @@ tap.test('approveAndMerge function', t => {
     const pullRequestNumber = 123
     const options = { excludePackages: [], approveOnly: true }
 
-    t.equal(await approveAndMerge(githubToken, appJWT, pullRequestNumber, options), 'Approving only')
+    t.equal(
+      await approveAndMerge(
+        mockRequest,
+        githubToken,
+        appJWT,
+        pullRequestNumber,
+        options
+      ),
+      'Approving only'
+    )
   })
 })
